e0878731d26ede49baacf028f98b0a517e104f7b
Akshat <akshatgoel700@gmail.com>
2025-10-05 12:09:38 +0530

    book now fixed

--- frontend/src/components/header/BookNowButton.jsx
+++ frontend/src/components/header/BookNowButton.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 const BookNowButton = () => {
   const navigate= useNavigate();
   return (
-    <a  href='#speciality'
+    <a  
       className="bg-slate-800 hover:bg-slate-900 text-white font-semibold flex items-center justify-center gap-2 py-3 px-6 rounded-full transition-all duration-300 shadow-md hover:shadow-lg max-w-xs"
     >
       <span className="text-lg">Book Now</span>
